3fd11d90b5444b273b363b12b882a000461a248c
duong.dinh <duong.dinh@edu.turkuamk.fi>
2020-08-01 23:24:29 +0300

    updates

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  space: "75mxxsup57bl",
+  accessToken: "YrcSpws41XHunZvFw8JdNO7tUnFRCokAmg50se70fxU",
 });
